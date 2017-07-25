450222e4b1a7addb603984b23d72676004f4db5d
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-07-25 11:58:50 +0000

    [kafka_consumer][test] traverse the result set properly.
    [kafka_consumer][test] prepare kafka offset consumer.
    
    [kafka_consumer][test] use `threading.Event()` for signaling.
    
    [kafka_consumer][test] set right tags.
    
    [kafka_consumer] just setup kafka consumer & producers once.
    
    [kafka_consumer][test] think before you paste.

--- kafka_consumer/test_kafka_consumer.py
+++ kafka_consumer/test_kafka_consumer.py
@@ -14,13 +14,12 @@ from nose import SkipTest
 from kafka import KafkaConsumer, KafkaProducer
 
 from kazoo.client import KazooClient
-from kazoo.exceptions import NoNodeError
 
 # project
 from tests.checks.common import AgentCheckTest
 
 
-instance = [{
+instances = [{
     'kafka_connect_str': '172.17.0.1:9092',
     'zk_connect_str': 'localhost:2181',
     # 'zk_prefix': '/0.8',
@@ -31,8 +30,11 @@ instance = [{
     }
 }]
 
-METRICS = [
+BROKER_METRICS = [
     'kafka.broker_offset',
+]
+
+CONSUMER_METRICS = [
     'kafka.consumer_offset',
     'kafka.consumer_lag',
 ]
@@ -40,15 +42,14 @@ METRICS = [
 TOPICS = ['marvel', 'dc']
 PARTITIONS = [0, 1]
 
-SHUTDOWN = False
+SHUTDOWN = threading.Event()
 
 class Producer(threading.Thread):
-    daemon = True
 
     def run(self):
-        producer = KafkaProducer(bootstrap_servers=instance[0]['kafka_connect_str'])
+        producer = KafkaProducer(bootstrap_servers=instances[0]['kafka_connect_str'])
 
-        while not SHUTDOWN:
+        while not SHUTDOWN.is_set():
             producer.send('marvel', b"Peter Parker")
             producer.send('marvel', b"Bruce Banner")
             producer.send('marvel', b"Tony Stark")
@@ -62,33 +63,33 @@ class Producer(threading.Thread):
             time.sleep(1)
 
 
-class Consumer(threading.Thread):
-    daemon = True
+class ZKConsumer(threading.Thread):
 
     def run(self):
         zk_path_topic_tmpl = '/consumers/my_consumer/offsets/'
-        zk_path_partition_tmpl = zk_path_topic_tmpl + '{topic}/'
+        zk_path_partition_tmpl = zk_path_topic_tmpl + '{topic}/{partition}'
 
-        zk_conn = KazooClient(instance[0]['zk_connect_str'], timeout=10)
+        zk_conn = KazooClient(instances[0]['zk_connect_str'], timeout=10)
         zk_conn.start()
 
         for topic in TOPICS:
             for partition in PARTITIONS:
-                node_path = os.path.join(zk_path_topic_tmpl.format(topic), str(partition))
+                node_path = zk_path_partition_tmpl.format(topic=topic, partition=partition)
                 node = zk_conn.exists(node_path)
                 if not node:
                     zk_conn.ensure_path(node_path)
+                    zk_conn.set(node_path, str(0))
 
-        consumer = KafkaConsumer(bootstrap_servers=instance[0]['kafka_connect_str'],
+        consumer = KafkaConsumer(bootstrap_servers=instances[0]['kafka_connect_str'],
                                  group_id="my_consumer",
                                  auto_offset_reset='earliest',
                                  enable_auto_commit=False)
-        consumer.subscribe()
+        consumer.subscribe(TOPICS)
 
-        while not SHUTDOWN:
+        while not SHUTDOWN.is_set():
             response = consumer.poll(timeout_ms=500, max_records=10)
             zk_trans = zk_conn.transaction()
-            for tp, records in response:
+            for tp, records in response.iteritems():
                 topic = tp.topic
                 partition = tp.partition
 
@@ -97,41 +98,56 @@ class Consumer(threading.Thread):
                     if offset is None or record.offset > offset:
                         offset = record.offset
 
-                zk_trans.set_data(
-                    os.path.join(zk_path_topic_tmpl.format(topic), str(partition)),
-                    offset
-                )
+                if offset:
+                    zk_trans.set_data(
+                        os.path.join(zk_path_topic_tmpl.format(topic), str(partition)),
+                        str(offset)
+                    )
 
             zk_trans.commit()
 
         zk_conn.stop()
+
+class KConsumer(threading.Thread):
+
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers=instances[0]['kafka_connect_str'],
+                                 group_id="my_consumer",
+                                 auto_offset_reset='earliest')
+        consumer.subscribe(TOPICS)
+
+        while not SHUTDOWN.is_set():
+            response = consumer.poll(timeout_ms=500, max_records=10)
 
 
 @attr(requires='kafka_consumer')
 class TestKafka(AgentCheckTest):
     """Basic Test for kafka_consumer integration."""
     CHECK_NAME = 'kafka_consumer'
+    THREADS = [Producer()]
 
     def __init__(self, *args, **kwargs):
         super(TestKafka, self).__init__(*args, **kwargs)
 
-        self.threads = [
-            Producer(),
-            Consumer()
-        ]
+        if os.environ.get('FLAVOR_OPTIONS','').lower() == "kafka":
+            self.THREADS.append(KConsumer())
+        else:
+            self.THREADS.append(ZKConsumer())
 
-    def setUp(self):
-	self.threads[0].start()
+    @classmethod
+    def setUpClass(cls):
+        cls.THREADS[0].start()
         time.sleep(45)
-	self.threads[1].start()
+        cls.THREADS[1].start()
         time.sleep(15)
 
-    def tearDown(self):
-        SHUTDOWN = True
-        for t in self.threads:
+    @classmethod
+    def tearDownClass(cls):
+        SHUTDOWN.set()
+        for t in cls.THREADS:
             t.join(5)
 
-    def test_check(self):
+    def test_check_zk(self):
         """
         Testing Kafka_consumer check.
         """
@@ -139,15 +155,23 @@ class TestKafka(AgentCheckTest):
         if os.environ.get('FLAVOR_OPTIONS','').lower() == "kafka":
             raise SkipTest("Skipping test - environment not configured for ZK consumer offsets")
 
-        self.run_check({'instances': instance})
+        self.run_check({'instances': instances})
 
-        for mname in METRICS:
-            self.assertMetric(mname, at_least=1)
+        for instance in instances:
+            for name, consumer_group in instance['consumer_groups'].iteritems():
+                for topic, partitions in consumer_group.iteritems():
+                    for partition in partitions:
+                        tags = ["topic:{}".format(topic),
+                                "partition:{}".format(partition)]
+                        for mname in BROKER_METRICS:
+                            self.assertMetric(mname, tags=tags, at_least=1)
+                        for mname in CONSUMER_METRICS:
+                            self.assertMetric(mname, tags=tags + ["consumer_group:{}".format(name)], at_least=1)
 
         self.coverage_report()
 
 
-    def test_check_nogroups(self):
+    def test_check_nogroups_zk(self):
         """
         Testing Kafka_consumer check grabbing groups from ZK
         """
@@ -155,14 +179,20 @@ class TestKafka(AgentCheckTest):
         if os.environ.get('FLAVOR_OPTIONS','').lower() == "kafka":
             raise SkipTest("Skipping test - environment not configured for ZK consumer offsets")
 
-        nogroup_instance = copy.copy(instance)
-        nogroup_instance[0].pop('consumer_groups')
-        nogroup_instance[0]['monitor_unlisted_consumer_groups'] = True
+        nogroup_instances = copy.deepcopy(instances)
+        nogroup_instances[0].pop('consumer_groups')
+        nogroup_instances[0]['monitor_unlisted_consumer_groups'] = True
 
-        self.run_check({'instances': instance})
+        self.run_check({'instances': nogroup_instances})
 
-        for mname in METRICS:
-            self.assertMetric(mname, at_least=1)
+        for instance in nogroup_instances:
+            for topic in TOPICS:
+                for partition in PARTITIONS:
+                    tags = ["topic:{}".format(topic),
+                            "partition:{}".format(partition)]
+                    for mname in BROKER_METRICS:
+                        self.assertMetric(mname, tags=tags, at_least=1)
+                    for mname in CONSUMER_METRICS:
+                        self.assertMetric(mname, tags=tags + ["consumer_group:my_consumer"], at_least=1)
 
         self.coverage_report()
-        raise Exception
